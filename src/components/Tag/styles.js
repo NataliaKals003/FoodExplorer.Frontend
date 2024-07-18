4450f51f44be000cb9aa3122a20fa6a5db7ad23e
Natalia Kals <nataliakals03@gmail.com>
2024-07-19 10:38:35 +1200

    Creating tags

---
+++ src/components/Tag/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.span`
+    padding: 0.3rem 0.8rem;
+    border-radius: 0.3rem;
+    margin-right: 0.37rem;
+    background: ${({ theme }) => theme.COLORS.DARK_1000};
+    color: ${({ theme }) => theme.COLORS.LIGHT_100} ;
+`;
