c1f045ac1220df3bf276e50d86ad513077ccb759
Natalia Kals <nataliakals03@gmail.com>
2024-08-08 10:48:06 +1200

    fixing issues

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,7 +6,33 @@ import { ButtonText } from '../../components/ButtonText';
 import { useNavigate } from 'react-router-dom';
 import { authRoutes } from '../../routes/routes';
 
+import { useState } from 'react';
+import { api } from '../../services/api.js';
+
 export function SignUp() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert("Preenha todos os campos")
+        }
+
+        api.post("/users", { name, email, password })
+            .then(() => {
+                alert("Usuario cadastrado com sucesso!")
+            })
+            .catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message);
+                } else {
+                    alert("Nao foi possivel cadastrar");
+                }
+            })
+    }
+
     const navigate = useNavigate();
 
     return (
@@ -23,12 +49,14 @@ export function SignUp() {
                         className="login"
                         placeholder="Exemplo: Maria da Silva"
                         type="text"
+                        onChange={e => setName(e.target.value)}
                     />
                     <span>Email</span>
                     <Input
                         className="login"
                         placeholder="Exemplo: exemplo@exemplo.com.br"
                         type="text"
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <span>Senha</span>
                     <Input
@@ -36,8 +64,9 @@ export function SignUp() {
                         placeholder="No mínimo 6 caracteres"
                         type="password"
                         maxLength={6}
+                        onChange={e => setPassword(e.target.value)}
                     />
-                    <Button title="Criar conta" />
+                    <Button title="Criar conta" onClick={handleSignUp} />
                     <ButtonText
                         onClick={() => navigate(authRoutes.signIn)}
                         className="createAcount"
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
-import axios from "axios";
+const axios = require("axios");
 
 const api = axios.create({
     baseURL: "http://localhost:2222"
 });
+
+module.exports = { api };
