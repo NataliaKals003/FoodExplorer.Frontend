27f45de9534c689535256da550bff38f02555003
Natalia Kals <nataliakals03@gmail.com>
2024-10-16 06:49:01 +1300

    creating an order screen

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,13 +1,118 @@
+import { useState } from 'react';
+import { Container, Content, MyOrderContainer, MyOrder, PaymentContainer, Payment } from './style.js';
 import { Header } from '../../components/Header/index.jsx';
 import { Footer } from '../../components/Footer/index.jsx';
-import { Container, Content } from './style.js';
+import pizza from '../../assets/Screenshot_19.png';
+import { ButtonText } from '../../components/ButtonText/index.jsx';
+import { MdPix } from 'react-icons/md';
+import { CiCreditCard1 } from 'react-icons/ci';
+import { BsQrCode } from 'react-icons/bs';
+import { PiReceiptBold } from 'react-icons/pi';
 
 export function Order() {
+    const [selectedPayment, setSelectedPayment] = useState(null);
+    const [paymentMethod, setPaymentMethod] = useState(null);
+    const [showQRCode, setShowQRCode] = useState(false);
+
+    const orders = [
+        { id: 1, name: 'Salada Radish', quantity: 1, price: `R$ ${48.9}`, image: pizza },
+        { id: 2, name: 'Pizza Margherita', quantity: 2, price: `R$ ${48.9}`, image: pizza },
+        { id: 3, name: 'Torta de Limão', quantity: 1, price: `R$ ${12.5}`, image: pizza },
+        { id: 4, name: 'Torta de Laranja', quantity: 1, price: `R$ ${12.5}`, image: pizza },
+    ];
+
+    function calculateTotal(orders) {
+        return orders
+            .reduce((total, order) => {
+                const priceNumber = parseFloat(order.price.replace('R$', '').trim());
+                return total + priceNumber * order.quantity;
+            }, 0)
+            .toFixed(2);
+    }
+
+    const totalPrice = calculateTotal(orders);
+
+    const handlePaymentMethod = (method) => {
+        setPaymentMethod(method);
+        setShowQRCode(method === 'pix'); // Mostra o ícone se PIX for selecionado
+    };
+
     return (
         <Container>
             <Header />
             <Content>
-                <h1>Order</h1>
+                <MyOrderContainer>
+                    <h1>My order</h1>
+                    {orders.map((order) => (
+                        <MyOrder key={order.id}>
+                            <img src={order.image} alt={order.name} />
+                            <div className="dish-wrap">
+                                <div className="dishDetails">
+                                    <h2>
+                                        {order.quantity} x {order.name}
+                                    </h2>
+                                    <span>{order.price}</span>
+                                </div>
+                                <ButtonText className="deleteDish" title="Delete" />
+                            </div>
+                        </MyOrder>
+                    ))}
+                    <span className="totalPrice">Total: R$ {totalPrice}</span>
+                </MyOrderContainer>
+                <PaymentContainer>
+                    <Payment>
+                        <h1>Payment</h1>
+                        <div className="payment-wrap">
+                            <div className="payment-options">
+                                <button
+                                    className={`payment-button ${paymentMethod === 'pix' ? 'selected' : ''}`}
+                                    onClick={() => handlePaymentMethod('pix')}
+                                >
+                                    <MdPix className="payment-icon" />
+                                    PIX
+                                </button>
+                                <button
+                                    className={`payment-button ${paymentMethod === 'credit' ? 'selected' : ''}`}
+                                    onClick={() => handlePaymentMethod('credit')}
+                                >
+                                    <CiCreditCard1 className="payment-icon" />
+                                    Crédito
+                                </button>
+                            </div>
+                            <hr className="divider-horizontal" />
+                            {showQRCode && (
+                                <div className="qr-code-container">
+                                    <BsQrCode className="qr-code" />
+                                </div>
+                            )}
+                            {paymentMethod === 'credit' && (
+                                <div className="credit-form">
+                                    <div className="input-group">
+                                        <label htmlFor="card-number">Card Number</label>
+                                        <input id="card-number" placeholder="0000 0000 0000 0000" type="text" />
+                                    </div>
+                                    <div className="cardDetails">
+                                        <div className="input-group">
+                                            <label htmlFor="card-expiry">Vality</label>
+                                            <input id="card-expiry" placeholder="04/25" type="text" />
+                                        </div>
+
+                                        <div className="input-group">
+                                            <label htmlFor="card-cvv">CVV</label>
+                                            <input id="card-cvv" placeholder="000" type="text" />
+                                        </div>
+                                    </div>
+
+                                    <button type="button">
+                                        {' '}
+                                        <PiReceiptBold size={30} />
+                                        Complete Payment
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </Payment>
+                </PaymentContainer>
             </Content>
             <Footer />
         </Container>
--- src/pages/Order/style.js
+++ src/pages/Order/style.js
@@ -9,8 +9,24 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-    flex: 1;
     margin: 2rem 7.6rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+
+    @media (max-width: ${LAYOUT_BREAKPOINTS.MD}) {
+        flex-direction: column;
+        margin: 3.5rem 2rem 7rem 2rem;
+
+        h1 {
+            margin-bottom: 1rem;
+        }
+    }
+`;
+
+export const MyOrderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
 
     > h1 {
         font-size: 2rem;
@@ -19,13 +35,213 @@ export const Content = styled.div`
         margin-bottom: 2.1rem;
     }
 
+    .totalPrice {
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        margin-top: 1rem;
+    }
+
     @media (max-width: ${LAYOUT_BREAKPOINTS.MD}) {
+        width: 100%;
+    }
+`;
+
+export const MyOrder = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 1rem;
+
+    img {
+        margin-right: 13px;
+        border-radius: 50%;
+        width: 72px;
+        height: 72px;
+    }
+
+    .dish-wrap {
         display: flex;
         flex-direction: column;
-        margin: 3.5rem 2rem 7rem 2rem;
+        align-items: self-start;
+        width: 100%;
+    }
 
-        h1 {
-            margin-bottom: 1rem;
+    .dishDetails {
+        display: flex;
+        align-items: center;
+
+        h2 {
+            font-family: 'Poppins', sans-serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 20px;
+            margin-right: 10px;
+        }
+
+        span {
+            font-size: 12px;
+            color: ${({ theme }) => theme.COLORS.LIGHT_400};
+        }
+    }
+
+    .deleteDish {
+        color: ${({ theme }) => theme.COLORS.TOMATO_300};
+        font-size: 12px;
+        font-weight: 500;
+        cursor: pointer;
+        line-height: 160%;
+    }
+`;
+
+export const PaymentContainer = styled.div`
+    width: 35%;
+    margin-left: 20px;
+
+    @media (max-width: ${LAYOUT_BREAKPOINTS.MD}) {
+        width: 100%;
+        margin-left: 0;
+    }
+`;
+
+export const Payment = styled.div`
+    h1 {
+        font-size: 2rem;
+        font-weight: 500;
+        font-family: 'Poppins', sans-serif;
+        margin-bottom: 2.1rem;
+    }
+
+    .payment-wrap {
+        border: 1px solid ${({ theme }) => theme.COLORS.LIGHT_600};
+        border-radius: 8px;
+        width: 530px;
+
+        .payment-options {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            position: relative;
+
+            .divider-vertical {
+                padding: 0;
+                width: 1px;
+                height: 100%;
+                background-color: ${({ theme }) => theme.COLORS.LIGHT_600};
+            }
+        }
+
+        .payment-button {
+            display: flex;
+            flex: 1;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            padding: 1.5rem;
+            background-color: ${({ theme }) => theme.COLORS.DARK_400};
+            color: ${({ theme }) => theme.COLORS.LIGHT_100};
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            transition:
+                background-color 0.3s,
+                box-shadow 0.3s;
+
+            &.selected {
+                background-color: ${({ theme }) => theme.COLORS.DARK_800};
+            }
+
+            &:first-child {
+                border-right: 1px solid ${({ theme }) => theme.COLORS.LIGHT_600};
+                border-radius: 8px 0 0 0;
+            }
+
+            .payment-icon {
+                width: 24px;
+                height: 24px;
+                margin-right: 4px;
+            }
+        }
+
+        .divider-horizontal {
+            padding: 0;
+            border: 0;
+            height: 1px;
+            background: ${({ theme }) => theme.COLORS.LIGHT_600};
+        }
+
+        .qr-code-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 3rem;
+            margin-bottom: 3rem;
+        }
+
+        .qr-code {
+            width: 250px;
+            height: 250px;
+        }
+
+        .credit-form {
+            display: flex;
+            flex-direction: column;
+            gap: 16px;
+            margin: 55px 40px;
+        }
+
+        .cardDetails {
+            display: flex;
+            gap: 1rem;
+        }
+
+        .input-group {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 12px;
+        }
+
+        .input-group label {
+            font-size: 1rem;
+            margin-bottom: 8px;
+            font-weight: 400;
+            color: ${({ theme }) => theme.COLORS.LIGHT_100};
+        }
+
+        .input-group input {
+            padding: 12px 14px;
+            font-size: 16px;
+            color: ${({ theme }) => theme.COLORS.LIGHT_100};
+            border: 1px solid ${({ theme }) => theme.COLORS.LIGHT_100};
+            border-radius: 5px;
+            background-color: transparent;
+            transition: border-color 0.3s ease;
+        }
+
+        .input-group input:focus {
+            border-color: ${({ theme }) => theme.COLORS.LIGHT_100};
+        }
+
+        button[type='button'] {
+            font-family: 'Poppins', sans-serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 11px;
+            padding: 12px 32px;
+            background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        button[type='button']:hover {
+            background-color: ${({ theme }) => theme.COLORS.TTOMATO_3400};
         }
     }
 `;
